aef14a24d66ad22d1aa5c4c4aae9177d12d37f7e
ishanbangale <61649856+ishanbangale@users.noreply.github.com>
2021-09-30 18:39:01 +0530

    Delete defines.php

--- defines.php
+++
@@ -1,4 +0,0 @@
-<?php
-	define( 'INSTAGRAM_APP_ID', '560855811634979' );
-	define( 'INSTAGRAM_APP_SECRET', 'b9e533ca2198ecb868a7e066e7456533' );
-	define( 'INSTAGRAM_APP_REDIRECT_URI', 'https://ishanbangale.github.io/' );
